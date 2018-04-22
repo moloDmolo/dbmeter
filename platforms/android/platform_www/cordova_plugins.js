b464dcc858616cb083b7ecbc248256d6953e1186
femoris <speedfreakmalay@gmail.com>
2018-04-22 13:46:13 +0530

    The one where I go to Tuhin's place to work

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "cordova-plugin-dbmeter.DBMeter",
+    "file": "plugins/cordova-plugin-dbmeter/dbmeter.js",
+    "pluginId": "cordova-plugin-dbmeter",
+    "clobbers": [
+      "DBMeter"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-dbmeter": "2.1.0",
+  "cordova-plugin-whitelist": "1.3.3"
+};
+// BOTTOM OF METADATA
+});
